b4f53d1e7aac1dce7f66708ff98da7fab2bed290
obaid-khan90 <muhammad.obaidullah@eusopht.com>
2026-06-02 11:31:17 +0500

    changes to trigger redeploy

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
       <PortfolioPreview />
       <Testimonials />
       <StatsSection />
-<FAQ />
+      <FAQ />
       <CTABanner />
       <ContactForm />
     </>
